258602b07025b8a9973d254df6e9aa215a458395
cherfaoui-syphax <syphax2000.cherfaoui@gmail.com>
2022-09-28 14:36:34 +0000

    ressponsive design

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -1,46 +1,59 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
+
 
 function HomeScreen() {
 
   return ( 
     <>
     <div className="home">
+
       <div className="circle-container" id="circle-container">
-      <div className="screen-1 screen">
-        <div className="large-circle">
-          <div className="circle-header">
-            <h1 className="circle-h1 circle-text">Reliable Same-Day Delivery & Courier Service</h1>
-            <h2 className="circle-h2 circle-text">Providing you peace of mind</h2>
-            <h4 className="circle-h4 circle-text">Deliver your packages and cargo faster, safer, on-time every time from door-to-door, across town or across the country</h4>
-          </div>
-          <a className="get-started-button" href="#"><button className="round-button large-circle-button">Get Started</button></a>
-          <div className="circle-video">
-            <video width="320" height="240" style={{width: '430px'}} loop autoPlay muted>
-              <source src="https://www.aaadeliveryservice.com/wp-content/uploads/2022/07/AAADeliveryService-Final-V2.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+        <div id="container-first-screen">
+          <div id="quarter-circle-bottom-left">
+            <div id="circle-container-home">
+              <h1 id = "reliable" class="circle-h1 circle-text">
+                Reliable Same-Day Delivery & Courier Service 
+	            </h1>
+
+              <h2 id = "providing"  class="circle-h2 circle-text">
+                Providing you peace of mind
+	            </h2>
+
+              <Box  sx={{ display: { xs: 'none', sm: 'block' } }}>
+
+              <h4 id = "deliver"  class="circle-h4 circle-text">
+                Deliver your packages and cargo faster, safer, on-time every time from door-to-door, across town or across the country
+              </h4>
+              </Box>
+
+              <div id="button-video">
+                <span class="b">
+                  <button id = 'home-first-button' class="round-button large-circle-button">
+                    Get Started
+                  </button>
+                </span>
+                <span class="b">
+                <Box  sx={{ display: { xs: 'none', md: 'inline', lg: 'inline' } }}  >
+
+                  <video id = "video" loop autoPlay muted>
+                    <source src="https://www.aaadeliveryservice.com/wp-content/uploads/2022/07/AAADeliveryService-Final-V2.mp4" type="video/mp4" />
+                      Your browser does not support the video tag.
+                  </video>
+                  </Box>
+
+                </span>
+
+              </div>
+            </div>
           </div>
         </div>
 
-        <div id="small-circle-container" className="small-circle-container">
-          <div className="circle small-circle small-circle-1">
-            Picture insert
-          </div>
-
-          <div className="circle small-circle small-circle-2">
-            Picture insert
-          </div>
-
-          <div className="circle small-circle small-circle-3">
-            Picture insert
-          </div>
-        </div>
-      </div>
 
 
-    <div className="screen screen-2">
+    <div className="screen screen-2" style={{height:'fit-content'}}>
       <div className="background"></div>
 
       <div className="top-semicircle semicircle-blue">
@@ -66,7 +79,7 @@ function HomeScreen() {
               <div className="content">
                 We provide on-demand, reliable same-day delivery service, delivering medical specimens, supplies, interoffice mail, and x-rays. Our drivers are in compliant with HIPPA guidelines and OSHA certified. Providing on-the-ground insights, streamlined PODs, tracking in real-time, geofencing to ensure driver is at delivery location. “You See What We See”.
               </div>
-              <div className="column-footer">
+              <div className="column-footer" style={{marginBottom:30}}>
                 <a className="button-link speak-to-a-specialist-link" href="#"><button className="round-button round-button-blue">Learn More</button></a>
               </div>
             </Grid>
@@ -91,7 +104,7 @@ function HomeScreen() {
               <div className="content">
                 We provide express, same-day delivery service, on time critical transportation to all 48 states nationwide. Expediting packages, any size shipments delivered within your city or across the country, same-day door-to-door delivery. GPS tracking in real time, adaptable logistics to fit your business needs. “Providing You Peace Of Mind”.
               </div>
-              <div className="column-footer">
+              <div className="column-footer" style={{marginBottom:150}}>
                 <a className="button-link speak-to-a-specialist-link" href="/speak-to-a-specialist"><button className="round-button round-button-blue">Learn More</button></a>
               </div>
             </Grid>
@@ -102,7 +115,7 @@ function HomeScreen() {
       </div>
     </div>
 
-    <div className="screen screen-3">
+    <div className="screen screen-3" style={{height:"fit-content"}}>
       <div className="top-semicircle semicircle-white">
           <h2 className="circle-h2">Why We Stand Out Above The Rest</h2>
       </div>
@@ -124,70 +137,18 @@ function HomeScreen() {
                       <li> ✓  Customer support around the clock 24/7/365</li>
                     </ul>
                 </div>
-                <div className="column-footer">
+                <div className="column-footer" style={{marginBottom:150}}>
                   <a className="button-link" href="/speak-to-a-specialist"><button className="round-button round-button">Get Started</button></a>
                 </div>
             </Grid>
 
 
-            <Grid item xs={12} md={4}>
-                <div className="moving-circles">
-                  <div 
-                    className="circle light-blue-bg medium-circle moving-circle-1"
-                    style={{
-                      position: 'absolute',
-                      top: 50,
-                      left: 0,
-                      zIndex: 5,
-                    }}
-                    >
-                    Moving circle 1
-                  </div>
-                  <div
-                  className="circle light-blue-bg medium-circle moving-circle-2"
-                  style={{
-                    position: 'absolute',
-                    top: 140,
-                    left: -50,
-                    zIndex: 4,
-                  }}
-                  >
-                    Moving circle 2
-                  </div>
-                  <div
-                  className="circle light-blue-bg medium-circle moving-circle-3"
-                  style={{
-                    position: 'absolute',
-                    top: 140,
-                    left: 150,
-                    zIndex: 2,
-                  }}
-                  >
-                    Moving circle 3
-                  </div>
-                  <div
-                  className="circle light-blue-bg medium-circle moving-circle-4"
-                  style={{
-                    position: 'absolute',
-                    top: 15,
-                    left: 100,
-                    zIndex: 1,
-                  }}
-                  >
-                    Moving circle 4
-                  </div>
-                  <div
-                  className="circle light-blue-bg medium-circle moving-circle-5"
-                  style={{
-                    position: 'absolute',
-                    top:210,
-                    left: 50,
-                    zIndex: 3,
-                  }}
-                  >
-                    Moving circle 5
-                  </div>
-                </div>
+            <Grid item xs={0} md={4}>
+            <Box component="span" sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}
+>
+            <img  style={{width:"100%"  }} src={require('../../assets/blue-circles.PNG')} alt="Blue Logo" />
+
+            </Box>
             </Grid>
 
             <Grid item xs={12} md={4}>
@@ -199,7 +160,7 @@ function HomeScreen() {
                       <li> ✓ Dependable service you can always count on, and professionalism on every delivery</li>
                     </ul>
                 </div>
-                <div className="column-footer">
+                <div className="column-footer" style={{marginBottom:150}}>
                   <a className="button-link speak-to-a-specialist-link" href="/speak-to-a-specialist"><button className="round-button round-button light-blue-bg">How It Works</button></a>
                 </div>
             </Grid>
@@ -210,7 +171,7 @@ function HomeScreen() {
         </div>
     </div>
 
-    <div className="screen screen-4">
+    <div className="screen screen-4" style={{height:"fit-content"}}>
         <div className="top-semicircle semicircle-blue">
             <h2 className="circle-h2">Adapting to your business</h2>
         </div>
@@ -223,14 +184,14 @@ function HomeScreen() {
                   <div className="content">
                     AAA is here to help and ready to step right in, whether you need to expand or supplement your fleet. Our fleet is ready to help you ramp up your operations, so you can meet the growing demands of your business.
                   </div>
-                  <div className="column-footer">
+                  <div className="column-footer" style={{marginBottom:120}}>
                     <a className="button-link" href="/speak-to-a-specialist"><button className="round-button round-button">Get Started</button></a>
                   </div>
               </Grid>
 
 
               <Grid item xs={12} md={4}>
-                  <video width="320" height="240" style={{margin: 'auto', width: '430px', display: 'block'}} loop autoPlay muted>
+                  <video   width="320" height="240" style={{margin: 'auto', width: '80%', display: 'block'}} loop autoPlay muted>
                     <source src="https://www.aaadeliveryservice.com/wp-content/uploads/2022/07/AAADeliveryService-Final-V2.mp4" type="video/mp4" />
                       Your browser does not support the video tag.
                   </video>
@@ -240,7 +201,7 @@ function HomeScreen() {
                   <div className="content">
                     Partner with us, let us become an extension of your company. We will reduce the cost of your aging fleet by supplementing or replacing your fleet with AAA fleet replacements, propelling your same-day delivery capabilities.
                   </div>
-                  <div className="column-footer">
+                  <div className="column-footer" style={{marginBottom:120}}>
                     <a className="button-link speak-to-a-specialist-link" href="/speak-to-a-specialist"><button className="round-button round-button light-blue-bg">How It Works</button></a>
                   </div>
               </Grid>
