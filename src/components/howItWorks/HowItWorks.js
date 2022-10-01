206a13a13500b9629f734a4f2b8b8b4a74de6b1f
cherfaoui-syphax <syphax2000.cherfaoui@gmail.com>
2022-10-01 02:26:12 +0000

    About us page

--- src/components/howItWorks/HowItWorks.js
+++ src/components/howItWorks/HowItWorks.js
@@ -8,6 +8,7 @@ import BlueLogo from "../../assets/logo-blue.png"
 import ScrollAnimation from 'react-animate-on-scroll';
 import "../../assets/animate.css";
 import back from '../../assets/blue-column.PNG';
+import BeforeFooter from "../BeforeFooter";
 function HowItWorks() {
   return (
     <>
@@ -156,43 +157,8 @@ function HowItWorks() {
 
 
 
-        <Container maxWidth="xl" disableGutters>
-
-      
-        <Grid container>
-          <Grid item xs={3.5} md={3.5}>
-                <ScrollAnimation animateIn='bounce' delay={300}
-                  initiallyVisible={true}
-                  animateOnce={true}>
-                    <div className="content">
-                      <img style={{width:"inherit", display:"block",margin:"auto"}}src={require('../../assets/warehouse-left.jpg')} alt="Blue Logo" />
-                    </div>
-                </ScrollAnimation>
-              </Grid>
-
-
-             
-              <Grid item xs={5} md={5}>
-              <ScrollAnimation animateIn='fadeIn zoomIn' delay={300}
-              animateOut='fadeOut'>
-                <div className="content">
-                  <img style={{width:"inherit", display:"block",margin:"auto"}}src={require('../../assets/plane.jpg')} alt="Blue Logo" />
-                </div>
-                </ScrollAnimation>
-              </Grid>
-            
-              
-              <Grid item xs={3.5} md={3.5}>
-                <ScrollAnimation animateIn='bounce' delay={300}
-                  initiallyVisible={true}
-                  animateOnce={true}>
-                    <div className="content">
-                      <img style={{width:"inherit", display:"block",margin:"auto"}}src={require('../../assets/warehouse-right.jpg')} alt="Blue Logo" />
-                    </div>
-                </ScrollAnimation>
-              </Grid>
-          </Grid>
-        </Container>
+        <BeforeFooter>
+        </BeforeFooter>
 
       </div>
 
