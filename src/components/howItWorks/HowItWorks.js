a11a0e2460303b2c066f8b29bb241bc88e5f75c8
cherfaoui-syphax <syphax2000.cherfaoui@gmail.com>
2022-10-18 14:40:16 +0000

    commit message

--- src/components/howItWorks/HowItWorks.js
+++ src/components/howItWorks/HowItWorks.js
@@ -22,13 +22,13 @@ function HowItWorks() {
               <h2 className="circle-h2 circle-text">
                 Seamlessly Place and Track Orders All In One Place
               </h2>
-              <img style={{width:"100%" , display:"block",margin:"auto"}}src={require('../../assets/logo-blue.png')} alt="Blue Logo" />
-              <h3 className="circle-h3 circle-text" >Get Started Here</h3>
-              <h4 className="circle-h4 circle-text" style={{color:"black"}}>
+              <img style={{width:"60%" , display:"block",margin:"auto"}}src={require('../../assets/logo-blue.png')} alt="Blue Logo" />
+              <h2 className="circle-h3 circle-text"  style={{textAlign:"left", color:"black"}} >Get Started Here</h2>
+              <p className=" circle-text" style={{textAlign:"left", color:"black"}}>
                 Put the power of AAA behind your shipments and deliveries, with a partner that’s here to meet the demands of
                 today, and help you navigate the road ahead. At AAA, we make it easy and convenient on every delivery. We take
                 the stress out of shipping, giving you more time to focus on your core business.
-              </h4>
+              </p>
           
               
           </Grid>
